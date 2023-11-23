28d06b994c10f7ae6f8219d9fd64459a02dc07c9
Stefano <romanelli1996@gmail.com>
2023-11-23 23:42:07 +0100

    code refactoring

--- public/script.js
+++ public/script.js
@@ -1,67 +1,59 @@
 document.addEventListener('DOMContentLoaded', function () {
   let ws = new WebSocket('wss://smart-perf-7d930c61dbd0.herokuapp.com:443');
 
-//Slider 1, Ogni volta che cambio un valore allo slider, li reinvia entrambi
 
-let controlTD = document.querySelector('.controlTD');
 
-controlTD.addEventListener('input', (event) =>{
+  let controlTD = document.querySelector('.controlTD');
+  let controlTD2 = document.querySelector('.controlTD2');
+
+
+  
+  ws.addEventListener('message', function (event) {
+    if (event.data) {
+          console.log(event.data)
+          let countContainer = document.getElementById ('controlTD')
+            const widgetParent = document.getElementById('countContainer');
+            widgetParent.innerHTML = event.data
+    }
+  });
+
+
+  
+  //Slider 1, Ogni volta che cambio un valore allo slider, invia la modifica
+  controlTD.addEventListener('input', (event) =>{
     console.log(controlTD.value);
-
-    ws.send(JSON.stringify({'slider1': controlTD.value,'slider2': controlTD2.value}))
-
-    // ws.send(JSON.stringify({'slider1': controlTD.value}))
-});
-
-
- ws.addEventListener('message', function (event) {
-    if (event.data) {
-         console.log(event.data)
-        let countContainer = document.getElementById ('controlTD')
-           const widgetParent = document.getElementById('countContainer');
-          widgetParent.innerHTML = event.data
-    }
-
-
-});
-
-//Slider 2, Ogni volta che cambio un valore allo slider, li reinvia entrambi
-
-let controlTD2 = document.querySelector('.controlTD2');
-
-controlTD2.addEventListener('input', (event) =>{
+    sliderChanged();
+  });
+  //Slider 2, Ogni volta che cambio un valore allo slider, invia la modifica
+  controlTD2.addEventListener('input', (event) => {
     console.log(controlTD2.value);
+    sliderChanged();
+  });
 
-    // ws.send(JSON.stringify({'slider2': controlTD2.value}))
+  
 
-    ws.send(JSON.stringify({'slider1': controlTD.value,'slider2': controlTD2.value}))
-});
+  function sliderChanged() {
+    let data = JSON.stringify({ 'slider1': controlTD.value, 'slider2': controlTD2.value });
+    websocketSender(data);
+  }
 
-
- ws.addEventListener('message2', function (event) {
-    if (event.data) {
-         console.log(event.data)
-        let countContainer = document.getElementById ('controlTD2')
-           const widgetParent = document.getElementById('countContainer');
-          widgetParent.innerHTML = event.data
-    }
-
-
-});
-
-
-
+  function websocketSender(json) {
+    ws.send(json);
+  }
 
 
   ws.addEventListener("message", (message) => {
     console.log(message);
+    window.alert(message);
   });
 
   ws.addEventListener("error", (error) => {
+    window.alert('websocket closed');
     console.log('websocket closed');
   });
 
   ws.addEventListener("close", (event) => {
+    window.alert('websocket closed');
     console.log('websocket closed');
   });
 });
