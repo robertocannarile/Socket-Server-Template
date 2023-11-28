d29f9a8760216424e96d9a028491f04cdd66c510
Stefano <romanelli1996@gmail.com>
2023-11-28 16:01:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -80,13 +80,19 @@ const broadcast = (ws, message, includeSelf) => {
 };
 
 
-// File getter
+// webapp folder getter
 app.use(express.static('public', {
   setHeaders: (res, path) => {
     
+    if (path.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    } else if (path.endsWith('.js')) {
+      res.setHeader('Content-Type', 'application/javascript');
+    }else if (path.endsWith('.html')) {
+      res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+    }
   },
 }));
-
 
 
 
