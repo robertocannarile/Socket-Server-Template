843516eb8032b922d02ce9e81b8608db1043404a
Stefano <romanelli1996@gmail.com>
2023-11-29 15:44:04 +0100

    Update experienceDataProvider.js

--- modules/experienceDataProvider.js
+++ modules/experienceDataProvider.js
@@ -12,6 +12,11 @@ const getMp3File = () => {
             const response = await fetch('https://www.stefanoromanelli.it/remoteAssets/sample.mp3');
             const buffer = await response.arrayBuffer();
             res.set('Content-Type', 'audio/mpeg');
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+            res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+            next();
+            
             res.send(buffer);
         } catch (error) {
             console.error(error);
