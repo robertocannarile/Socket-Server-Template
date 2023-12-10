0faacb07577abc3df4b1bd371ea4cee8ff7a3865
Stefano <romanelli1996@gmail.com>
2023-12-10 22:56:32 +0100

    Update experienceDataProvider.js

--- modules/experienceDataProvider.js
+++ modules/experienceDataProvider.js
@@ -6,10 +6,29 @@ const path = require('path');
 
 const tracksFolderPath = path.join(__dirname, 'tracks');
 const randomicTracksFolderPath = path.join(tracksFolderPath, 'randomic_tracks');
+
+const gloablTracksFolderPath = path.join(tracksFolderPath, 'global_tracks');
+
 const serverDomain = 'https://smart-perf-7d930c61dbd0.herokuapp.com';
 
-// Leggi la directory
+// Leggi la directory delle randomic traks
 fs.readdir(randomicTracksFolderPath, (err, files) => {
+    if (err) {
+        console.error('Errore nella lettura della directory di randomic_tracks:', err);
+        return;
+    }
+
+    // Itera sui file
+    files.forEach((file) => {
+        // Crea il link per il file
+        const fileLink = `${serverDomain}/download/${encodeURIComponent(file)}`;
+        console.log('Link per scaricare il file:', fileLink);
+    });
+
+});
+
+// Leggi la directory delle global traks
+fs.readdir(gloablTracksFolderPath, (err, files) => {
     if (err) {
         console.error('Errore nella lettura della directory di randomic_tracks:', err);
         return;
