d0ccf124525777584471cea6866d2942ffd90ba1
Stefano <romanelli1996@gmail.com>
2023-12-10 22:58:52 +0100

    Update experienceDataProvider.js

--- modules/experienceDataProvider.js
+++ modules/experienceDataProvider.js
@@ -1,5 +1,4 @@
 // modules/experienceDataProvider.js
-const express = require("express");
 
 const fs = require('fs');
 const path = require('path');
@@ -42,35 +41,3 @@ fs.readdir(gloablTracksFolderPath, (err, files) => {
     });
 
 });
-
-
-const getMp3File = () => {
-    const app = express();
-
-    app.get('/mp3', async (req, res) => {
-        try {
-
-            // Ottieni l'URL del file dalla query della richiesta
-            const fileUrl = req.query.url;
-
-
-            // Utilizza l'importazione dinamica invece di require
-            const fetch = (await import('node-fetch')).default;
-
-            const response = await fetch(fileUrl);
-            const buffer = await response.buffer();
-            res.set('Content-Type', 'audio/mpeg');
-            res.send(buffer);
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('Internal Server Error');
-        }
-    });
-
-    return app;
-};
-
-
-
-
-module.exports = getMp3File;
