41ff04a588ab8a4ad617b3024ea2d8737b76a517
Stefano <romanelli1996@gmail.com>
2024-02-03 01:43:57 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -95,8 +95,9 @@ document.addEventListener('DOMContentLoaded', function () {
   allowMediaContentButtonAndDownload.addEventListener('click', async function () {
     allowMediaContentButtonAndDownload.disabled = true;
     overlay.classList.add('hidden');
-    await allowAudioContextAndDownloadAudioBuffers();
     document.querySelector('video').play();
+    await allowAudioContextAndDownloadAudioBuffers();
+    
   });
 
 
